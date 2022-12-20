f056927a06fb00755906d012f816c1c6b179e60e
ananya1089 <ananya.dutta@fedev.cbnits.com>
2022-12-20 12:21:29 +0530

    completed task

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,9 +1,12 @@
+from pprint import pprint
+
 from selenium.webdriver.common.by import By
 
 from page_obj.Flight_details import Flight_details
 from page_obj.Home_page import Home_page
 from page_obj.search_flights import Search_Flight
 from utilities.BaseClass import BaseClass
+
 
 
 class Test_Main(BaseClass):
@@ -23,26 +26,36 @@ class Test_Main(BaseClass):
         flight_details = Flight_details(self.driver)
         # flight_name = flight_details.get_list_of_flights()
         listofFlights = self.driver.find_elements(By.XPATH, "//div[@class='makeFlex align-items-center gap-x-10 airline-info-wrapper']")
+        f_name=[]
         for flights in listofFlights:
             names=flights.text
-
-            print(names)
+            # print(names)
+            f_name.append(names)
 
         arrival_time = self.driver.find_elements(By.XPATH, "//div[@class='flexOne timeInfoLeft']")
+        f_arr_time =[]
         for time in arrival_time:
            arr_time = time.text
+
+           f_arr_time.append(arr_time)
+
+
         duration = self.driver.find_elements(By.XPATH, "//div[@class='stop-info flexOne']")
+        f_duration_time = []
         for f_duration in duration:
            du_time = f_duration.text
+           f_duration_time.append(du_time)
         dep_time = self.driver.find_elements(By.XPATH, "//div[@class='flexOne timeInfoRight']")
+        f_dep_time = []
         for dept_time in dep_time:
             d_time=dept_time.text
+            f_dep_time.append(d_time)
 
-        dic = {"name":names,
-             "arrival_time":arr_time,
-             "duration":du_time,
-             "dep_time":d_time}
-        print(dic)
+        dic = {"name":f_name,
+             "arrival_time":f_arr_time,
+             "duration":f_duration_time,
+             "dep_time":f_dep_time}
+        pprint(dic)
 
 
 
